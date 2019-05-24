6910902fbe4668024d18ca2801bc4cbd42ead98e
Ryan J <rijoseph89@gmail.com>
2019-05-23 21:26:36 -0400

    updating table

--- static/js/app.js
+++ static/js/app.js
@@ -14,12 +14,36 @@ var chartMargin = {
 var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
 var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
 
-function buildTable(stat) {
-  d3.json().then((data) => {
-    buildTable(data);
-
-
-  });
+function buildTable(round) {
+//   d3.json(`/statsTable/${round}`).then((data) => {
+//     var svg = d3.select("#roundtable")
+//       .html("")
+//       .append("svg")
+//       .attr("height", svgHeight)
+//       .attr("width", svgWidth);
+//     var tableData = data
+//
+//   var table = new Tabulator("roundtable", {
+//     data:tableData, //set initial table data
+//     columns:[
+//         {title:"Player", field:"Player"},
+//         {title:"Year Drafted", field:"Year_Drafted"},
+//         {title:"Round Drafted", field:"Round_Drafted"},
+//         {title:"Overall Pick", field:"Overall_Pick"},
+//         {title:"Avg Attempts", field:"Avg_Attempts"},
+//         {title:"Avg. Completions", field:"Avg_Completions"},
+//         {title:"Avg. Passing Yards", field:"Avg_Passing_Yards"},
+//         {title:"Avg. Yards/Attempt", field:"Avg_Yards_per_Attempt"},
+//         {title:"Avg. TDs", field:"Avg_TDs"},
+//         {title:"Avg. Sacks", field:"Avg_Sacks"},
+//         {title:"Avg. Loss of Yards", field:"Avg_Loss_of_Yards"},
+//         {title:"Avg. QBR", field:"Avg_QBR_REAL"},
+//         {title:"Avg. Points", field:"Avg_Points"},
+//         {title:"Total Games Played", field:"Game_Total"},
+//     ],
+// });
+//
+//   });
 };
 
 function init() {
@@ -46,18 +70,12 @@ function init() {
  });
 }
 
-//  d3.json('/bar').then((barData) => {
-//    // list of stats = +list of stats
-//    console.log(barData["1"]["Avg_Attempts"]);
-//  });
-// }
-
 function optionChanged(newData) {
   // Fetch new data each time a new sample is selected
   var stringData = String(newData)
   if (stringData == "1" || stringData == "2" || stringData == "3" || stringData == "4" || stringData == "5" || stringData == "6" || stringData == "7") {
     buildBar(stringData)
-    // buildTable(stringData)
+    buildTable(stringData)
   } else {
     //PlayerData(newData);
     PlayerData(stringData);
@@ -196,30 +214,8 @@ function PlayerData(playerData) {
 
   svg = d3.select("#roundbar")
     .html("")
-
-
-// Append a group area, then set its margins
-// var chartGroup = svg.append("g")
-//   .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
-
-// Configure a parseTime function which will return a new Date object from a string
-//var parseTime = d3.timeParse("%Y");
-
-// Configure a line function which will plot the x and y coordinates using our scales
-  // var drawLine = d3.line()
-  //   .x(data => xTimeScale(data.Year))
-  //   .y(data => yLinearScale(data.QRB));
-
-  // // Configure a time scale
-  // // d3.extent returns the an array containing the min and max values for the property specified
-  // var xTimeScale = d3.scaleBand()
-  //   .domain(d3.extent(playerProfile.map(data => data.Year)))
-  //   .range([0, chartWidth]);
-
-  // // Configure a linear scale with a range between the chartHeight and 0
-  // var yLinearScale = d3.scaleLinear()
-  //   .domain([0, d3.max(playerProfile, d => d.QRB)])
-  //   .range([chartHeight, 0]);
+  svg2 = d3.select("roundtable")
+    .html("")
 
   var yeardata = [];
   var QBRdata = [];
@@ -261,113 +257,9 @@ Plotly.newPlot("roundbar", data1, layout);
 });
 
 
-
-    // Create two new functions passing the scales in as arguments
-  // These will be used to create the chart's axes
-  // var bottomAxis = d3.axisBottom(xTimeScale);
-  // var leftAxis = d3.axisLeft(yLinearScale);
-
-
-  // Append an SVG path and plot its points using the line function
-  // chartGroup.append("path")
-
-  // // The drawLine function returns the instructions for creating the line for forceData
-  //   .attr("d", drawLine(playerProfile))
-  //   .classed("line", true);
-
-  // // Append an SVG group element to the chartGroup, create the left axis inside of it
-  // chartGroup.append("g")
-  //   .classed("axis", true)
-  //   .call(leftAxis);
-
-  // // Append an SVG group element to the chartGroup, create the bottom axis inside of it
-  // // Translate the bottom axis to the bottom of the page
-  // chartGroup.append("g")
-  //   .classed("axis", true)
-  //   .attr("transform", `translate(0, ${chartHeight})`)
-  //   .call(bottomAxis);
 });
 
 };
 
-// set the dimensions and margins of the graph
-// var margin = {top: 20, right: 20, bottom: 30, left: 50},
-//     width = 960 - margin.left - margin.right,
-//     height = 500 - margin.top - margin.bottom;
 
-// // parse the date / time
-// var parseTime = d3.timeParse("%Y");
-
-// // set the ranges
-// var x = d3.scaleTime().range([0, width]);
-// var y = d3.scaleLinear().range([height, 0]);
-
-// // define the line
-// var valueline = d3.line()
-//     .x(function(d) { return x(d.Year); })
-//     .y(function(d) { return y(d.QBR); });
-// // define the line
-// var valueline2 = d3.line()
-//     .x(function(d) { return x(d.Year); })
-//     .y(function(d) { return y(d.QBR); });
-
-// // append the svg obgect to the body of the page
-// // appends a 'group' element to 'svg'
-// // moves the 'group' element to the top left margin
-// var svg = d3.select("#roundbar").append("svg")
-//     .attr("width", width + margin.left + margin.right)
-//     .attr("height", height + margin.top + margin.bottom)
-//   .append("g")
-//     .attr("transform",
-//           "translate(" + margin.left + "," + margin.top + ")");
-
-// function draw(data, playerData) {
-
-//   var data = data[playerData];
-
-//   console.log(playerData);
-
-//   // format the data
-//   data.forEach(function(d) {
-//       d.Year = parseTime(d.Year);
-//       d.QBR = +d.QBR;
-//       //d.Exports = +d.Exports;
-//   });
-
-//   // sort years ascending
-//   data.sort(function(a, b){
-//     return a["Year"]-b["Year"];
-// 	})
-
-//   // Scale the range of the data
-//   x.domain(d3.extent(data, function(d) { return d.Year; }));
-//   y.domain([0, d3.max(data, function(d) {
-// 	  return Math.max(d.QBR); })]);
-
-//   // Add the valueline path.
-//   svg.append("path")
-//       .data([data])
-//       .attr("class", "line")
-//       .attr("d", valueline);
-//   // Add the valueline path.
-//   // svg.append("path")
-//   //     .data([data])
-//   //     .attr("class", "line")
-//   //     .attr("d", valueline2);
-//   // Add the X Axis
-//   svg.append("g")
-//       .attr("transform", "translate(0," + height + ")")
-//       .call(d3.axisBottom(x));
-
-//   // Add the Y Axis
-//   svg.append("g")
-//       .call(d3.axisLeft(y));
-//   }
-// // Get the data
-// d3.json(`/line/${stringData}`, function(error, data) {
-//   if (error) throw error;
-
-//   // trigger render
-//   draw(data, stringData);
-// });
 init();
